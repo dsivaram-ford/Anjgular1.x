cdae60a7f11e6218d346577742fd4468ce7e7ab8
D <DSIVARAM@ford.com>
2016-09-01 12:58:08 +0530

    table added

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 var app = angular.module('myapp',[]);
 
-app.controller('myControl',['$scope',function($scope){
+app.controller('myControl',['$scope','myService',function($scope,myService  ){
   $scope.test = "Welcome  to angular";
-}]); 
+  $scope.users = myService.getUser();
+}]);
+
+app.factory('myService',[function(){
+  function getUser(){
+    return [
+      {
+        name:'siva',
+        age:28
+      },
+      {
+        name:'ram',
+        age:27
+      },
+    ];
+  }
+  return{
+    getUser:getUser
+  };
+}]);
