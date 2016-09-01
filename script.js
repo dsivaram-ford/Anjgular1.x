465d468f893f64c647896f8fc56e20e4899a03eb
D <DSIVARAM@ford.com>
2016-09-01 15:42:49 +0530

    added custom directive

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 var app = angular.module('myapp',[]);
 
-app.controller('myControl',['$scope','myService',function($scope,myService  ){
+app.controller('myControl',['$scope','myService',function($scope,myService){
   $scope.test = "Welcome  to angular";
   $scope.users = myService.getUser();
+}]);
+
+app.directive('myDirective',[function () {
+  return{
+    restrict:'AE',
+    template:"<h1>This is custome element</h1>"
+  };
 }]);
 
 app.factory('myService',[function(){
